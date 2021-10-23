12e744516011551724218c57a3e5cd24da744ae7
lifeofperfect <perfect.igbadumhe@gmail.com>
2021-10-24 00:36:02 +0100

    setup project

---
+++ AspNetIdentityDemo.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using AspNetIdentityDemo.Api.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetIdentityDemo.Api.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions opt): base(opt)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
